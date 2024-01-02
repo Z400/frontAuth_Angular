b68042c634391681aee1935bc5eae9b4c868bf89
VIANA <matheusgoldship@gmail.com>
2024-01-02 16:51:36 -0300

    Hands-On 7

--- src/app/core/components/auth/pages/sign/sign.component.ts
+++ src/app/core/components/auth/pages/sign/sign.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign',
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignComponent implements OnInit {
 
-  constructor() { }
+   
+
+  constructor(private formBuilder: FormBuilder) { }
+
+ public formAuth: FormGroup = this.formBuilder.group({
+  email: ['', [Validators.required, Validators.email]],
+  password: ['', [Validators.required]]
+ })
+  
+ public submitForm () {
+  if(this.formAuth.valid) {
+    console.log(this.formAuth);
+  }
+
+ }
+
 
   ngOnInit(): void {
   }
